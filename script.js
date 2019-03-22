65b556548c292ebb2fdd41005b0569557d1c9f2b
megmotion <33497517+megmotion@users.noreply.github.com>
2019-03-22 10:07:37 +0100

    contact form

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("it works")
+
+
+$(document).ready(function() {
+
+	$('.button').click(function (event) {
+		console.log('Clicked button')
+
+		/*var name = $('#name').val()
+		var email = $('#email').val()
+		var subject = $('#subject').val()
+		var message = $('#message').val()
+
+		if(email.length > 5 && email.includes("@") && email.includes(".")) {
+			console.log('Email is valid')
+		} else {
+			console.log('Email is not valid')
+		}*/
+	})
+})
